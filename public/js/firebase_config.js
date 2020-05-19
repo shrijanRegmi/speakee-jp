9530842e2330b0b02001012a52dba598436df4d6
Shrijan Regmi <ilyyhs9@gmail.com>
2020-05-19 15:28:28 +0545

    firebase project shifted

--- public/js/firebase_config.js
+++ public/js/firebase_config.js
@@ -1,10 +1,11 @@
- var firebaseConfig = {
-    apiKey: "AIzaSyDI9biTrbGWWfpWr58OI-FvmBo61Ud9fGQ",
-    authDomain: "japanese-landing-page.firebaseapp.com",
-    databaseURL: "https://japanese-landing-page.firebaseio.com",
-    projectId: "japanese-landing-page",
-    storageBucket: "japanese-landing-page.appspot.com",
-    messagingSenderId: "344789923232",
-    appId: "1:344789923232:web:92aa850a5a986475cde0c5"
-  };
-  firebase.initializeApp(firebaseConfig);
+var firebaseConfig = {
+  apiKey: "AIzaSyD2hDxgRYvrCMBB9LxrcpbrwqKIUVTlTH4",
+  authDomain: "speakee-website.firebaseapp.com",
+  databaseURL: "https://speakee-website.firebaseio.com",
+  projectId: "speakee-website",
+  storageBucket: "speakee-website.appspot.com",
+  messagingSenderId: "898959904720",
+  appId: "1:898959904720:web:c7b355a6b0d721683833d5",
+  measurementId: "G-Z95VLF49GD",
+};
+firebase.initializeApp(firebaseConfig);
